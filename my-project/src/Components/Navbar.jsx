813b6791814568e9b59157ef00d44bb942deb32c
arabiy <arabiy@gmail.com>
2024-12-26 18:42:13 +0500

    yubordim

--- my-project/src/Components/Navbar.jsx
+++ my-project/src/Components/Navbar.jsx
@@ -1,126 +1,222 @@
-// import React from 'react';
-// import ee from '../assets/ee.png';
+import React from 'react';
+import eee from '../assets/ee.png';
+import '../Components/Main.css';
 
-
-
-// function Navbar() {
-//     return(
+function Navbar() {
+    return(
         
-//         <div>
-           
+        <div>
+            
 
-// {/* <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-//   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-//   <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-//   <img src={ee}  class="h-8" alt=" Flowbite Logo" />
-//       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Islam</span>
-//   </a>
- 
-//   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-//     <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-//       <li>
-//       <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Dua</a>
-//       </li>
-//       <li>
-//         <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Zikr</a>
-//       </li>
-//       <li>
-//         <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Fiqh</a>
-//       </li>
-//       <li>
-//         <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">H.Z live</a>
-//       </li>
-//     </ul>
-//   </div>
-//   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-//       <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Get started</button>
-//       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-     
-//         <span class="sr-only">Open main menu</span>
-//         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-//             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-//         </svg>
-//     </button>
-//   </div>    
-//   </div>
-// </nav> */}
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
+        <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={eee} class="h-8" alt="Islam logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Islam</span>
+        </a>
+        
+        <div id="mega-menu" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+            <ul class="flex flex-col mt-4 font-medium md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
+                <li>
+                    <a href="#" class="block py-2 px-3 text-green-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-600 md:p-0 dark:text-green-500 md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-green-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Asosiy</a>
+                </li>
+                <li>
+                    <button id="mega-menu-dropdown-button" data-dropdown-toggle="mega-menu-dropdown" class="flex items-center justify-between w-full py-2 px-3 font-medium text-gray-700 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-600 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-green-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                        Fiqh <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg>
+                    </button>
+                    <div id="mega-menu-dropdown" class="absolute z-10  hidden w-auto grid-cols-2 text-sm bg-white border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
+                        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+                            <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        About Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Library
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Resources
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Pro Version
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+                            <ul class="space-y-4">
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Blog
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Newsletter
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Playground
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        License
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="p-4">
+                            <ul class="space-y-4">
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Contact Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Support Center
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Terms
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 px-3 text-gray-600  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-600 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-green-500 md:dark:hover:bg-transparent dark:border-gray-700">Dua</a>
+                </li>
+                <li>
+                    <a href="#" class="block py-2 px-3 text-gray-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-600 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-green-500 md:dark:hover:bg-transparent dark:border-gray-700">Zikr</a>
+                </li>
+            </ul>
+        </div>
+        <div class="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <a href="#" class="text-gray-700 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a>
+            <a href="#" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Sign up</a>
+            <button data-collapse-toggle="mega-menu" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+        </div>
+    </div>
+</nav>
+
+{/* Navbar tugadi */}
+
+
+<div>
 
 
 
-
-    
-
-// {/* <div id="default-carousel" class="relative w-full" data-carousel="slide">
+<section className='fff'>
    
-//     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-       
-//         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-//         <img src="https://pmubuc.s3.amazonaws.com/blog/Top_Things_to_Do_in_Makkah_during_Umrah.jpg"  class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-//         </div>
-     
-//         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-//         <img src="https://images.prismic.io/mystique/db097308-1729-44ef-bee9-6923aeb71411_c3979edd3e36dd0a8b6de4c7ce879589-22487-istanbul-blue-mosque-tour-03.jpg?auto=format,compress" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-//         </div>
-       
-//         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-//         <img src="https://images.prismic.io/mystique/db097308-1729-44ef-bee9-6923aeb71411_c3979edd3e36dd0a8b6de4c7ce879589-22487-istanbul-blue-mosque-tour-03.jpg?auto=format,compress" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-//         </div>
-       
-//         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-//         <img src="https://images.prismic.io/mystique/db097308-1729-44ef-bee9-6923aeb71411_c3979edd3e36dd0a8b6de4c7ce879589-22487-istanbul-blue-mosque-tour-03.jpg?auto=format,compress" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-//         </div>
-       
-//         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-//         <img src="https://images.prismic.io/mystique/db097308-1729-44ef-bee9-6923aeb71411_c3979edd3e36dd0a8b6de4c7ce879589-22487-istanbul-blue-mosque-tour-03.jpg?auto=format,compress" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="" />
-//         </div>
-//     </div>
-   
-//     <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-//         <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-//         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-//         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-//         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-//         <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-//     </div>
-    
-//     <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-//         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-//             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-//                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-//             </svg>
-//             <span class="sr-only">Previous</span>
-//         </span>
-//     </button>
-//     <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-//         <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-//             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-//                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-//             </svg>
-//             <span class="sr-only">Next</span>
-//         </span>
-//     </button>
-// </div> */}
+        <div class="fr  mb-4 text-4xl font-extrabold tracking-tight leading-none text-white v md:text-5xl lg:text-6xl"> 
+        We believe that Islam is the true religion.
 
 
-// {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">  */}
+        <p class="fq text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        
+        </div>
+        
+        <div className='fds'></div>
+</section>
 
-// <div class="font-semibold text-9xl text-center ">Dua</div>
 
-//     <div class="font-semibold text-7xl text-center mt-24">
-// <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-//     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"  src="https://c7c8edde.rocketcdn.me/wp-content/uploads/shutterstock_85420666.jpg" alt="" />
-//     <div class="flex flex-col justify-between p-4 leading-normal">
-//         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-//         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-//     </div>
-// </a>
-// </div>
-    
 
-//         </div>
+</div>
+<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+{/* carusl tugadi */}
+<div class="flex justify-center gas">
+    <div className='dua rounded-xl'>
+        <img class="rounded-xl" src="https://c7c8edde.rocketcdn.me/wp-content/uploads/shutterstock_85420666.jpg" alt="" />
+    </div>
+    <div>
+<a href="#" class="block max-w-sm p-6 bg-green-100 border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 mr-8 dark:text-white">Dua</h5>
+<p class="font-normal text-gray-700 dark:text-gray-400">
+"Allahumma anta rabbi la ilaha illa anta, Khalaqtani wa ana abduka wa ana ala ahdika wa wadika ma astatat. Audhu bika min sharri ma sanatu, abu'u laka bi nimatika `alayya wa abu'u laka bi dhanbi, faghfir li fa innahu la yaghfirudh-dhunub illa anta"
+"Allahumma la sahla illa ma ja'altahu sahla, wa 'anta taj'alul hazna idha shi'ta sahla" 
+Morning
+"As-bah-na wa as-ba-hal mulku lil-lahi rabbil-aa-la-mîn. Allahumma innî as'aluka khay-ra ha-zal-yawm, fat-ha-hu wa nas-ra-hu, wa nu-ra-hu wa bara-ka-ta-hu wa hu-da-hu" 
+"Allahumma innee a3uzubika min alham wa alhuzn wa al3ajz wa alkusl wa albukhl wa aljubn wa galbah aldayn wa Galbah alrijaal" 
+"Bismillahi tawakkaltu 'ala Allah, la hawla wa la quwwata illa billah" </p>
+</a>
+</div>
+</div>
+
+
+<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+{/* duo tugadi */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
       
-//     )
-// }
+    )
+}
     
-//     export default Navbar;
+    export default Navbar;
